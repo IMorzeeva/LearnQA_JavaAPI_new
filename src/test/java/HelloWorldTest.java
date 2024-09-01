e811bca8f44fc52956ea5ecf38b247aa5e7cb1e5
IMorzeeva <inna_001@mail.ru>
2024-09-01 14:08:31 +0300

    Redirect

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -8,7 +9,7 @@ import java.util.*;
 public class HelloWorldTest {
 
     @Test
-    public void testRestAssured(){
+    public void testTheSecondmessage(){
         JsonPath response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
@@ -16,5 +17,22 @@ public class HelloWorldTest {
         ArrayList<LinkedHashMap<String,String>> messages = response.get("messages");
 
         System.out.println(messages.get(1).get("message"));
+    }
+
+    @Test
+
+    public void testUrlRedirect(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        response.prettyPrint();
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+
     }
 }
