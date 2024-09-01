d88a0726cdf503f6f0ad3216f670fd9b499a02b2
IMorzeeva <inna_001@mail.ru>
2024-09-01 16:46:13 +0300

    Redirect

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -35,4 +35,34 @@ public class HelloWorldTest {
         System.out.println(locationHeader);
 
     }
+
+    @Test
+    public void testLongRedirect(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        int statusCode = response.getStatusCode();
+        String locationHeader = response.getHeader("Location");
+
+        while (statusCode == 301){
+            response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(locationHeader)
+                    .andReturn();
+
+            locationHeader = response.getHeader("Location");
+            statusCode = response.getStatusCode();
+
+            System.out.println("Status: " + statusCode + " URL:" + locationHeader);
+        }
+    }
+
 }
