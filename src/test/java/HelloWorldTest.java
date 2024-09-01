d3a89d1d0f686f2098b4be4d410cb99b811d53a1
IMorzeeva <inna_001@mail.ru>
2024-09-01 11:56:51 +0300

    The second message

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        System.out.println("Hello from Inna");
+    public void testRestAssured(){
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        ArrayList<LinkedHashMap<String,String>> messages = response.get("messages");
+
+        System.out.println(messages.get(1).get("message"));
     }
 }
