f9aeecba45d0f8bbffbcfe6fe436cf330aeb27da
IMorzeeva <inna_001@mail.ru>
2024-09-16 16:14:24 +0300

    TestCreateUserWithUncorrectEmail

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -1,4 +1,25 @@
 package lib;
 
+import io.restassured.response.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Assertions {
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer){
+        assertEquals(
+                expectedAnswer,
+                Response.asString(),
+                "Response text is not expected"
+        );
+    }
+
+        public static void assertResponseCodeEquals(Response Response, int expectedStatusCode){
+            assertEquals(
+                    expectedStatusCode,
+                    Response.statusCode(),
+                    "Response status code is not expected"
+            );
+        }
+
+
 }
